fea66a714538c72c5c6e234caa4d492af41c5036
pawel054 <pawel.marcisz1@gmail.com>
2024-01-10 11:45:23 +0100

    dodano obsługę przycisków

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -24,17 +24,76 @@ namespace Calculator
 
         private void BtnNumberClicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (operation == EnumOpertion.None)
+                {
+                    if (comma && !a.ToString().Contains("."))
+                        a = double.Parse(a.ToString() + "." + ((Button)sender).Text);
+                    else
+                        a = double.Parse(a.ToString() + ((Button)sender).Text);
+                    comma = false;
+                }
+                else
+                {
+                    if (comma && !b.ToString().Contains("."))
+                        b = double.Parse(b.ToString() + "." + ((Button)sender).Text);
+                    else
+                        b = double.Parse(b.ToString() + ((Button)sender).Text);
+                    comma = false;
+                }
+                Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         private void BtnOperationCLicked(object sender, EventArgs e)
         {
-
+            comma = false;
+            if (operation != EnumOpertion.None)
+                return;
+            switch (((Button)sender).Text)
+            {
+                case "+":
+                    operation = EnumOpertion.Add;
+                    break;
+                case "-":
+                    operation = EnumOpertion.Subtract;
+                    break;
+                case "x":
+                    operation = EnumOpertion.Multiply;
+                    break;
+                case "/":
+                    operation = EnumOpertion.Divide;
+                    break;
+                case "x^2":
+                    operation = EnumOpertion.Square;
+                    result = a * a;
+                    break;
+                case "sqrt":
+                    operation = EnumOpertion.SquareRoot;
+                    result = Math.Sqrt(a);
+                    break;
+                case "1/x":
+                    operation = EnumOpertion.Inverse;
+                    result = 1 / a;
+                    break;
+            }
+            Print();
         }
 
         private void ClearClick(object sender, EventArgs e)
         {
-
+            a = 0;
+            b = 0;
+            result = 0;
+            operation = EnumOpertion.None;
+            comma = false;
+            btnEqualsClicked = false;
+            Print();
         }
 
         private void BtnCommaClicked(object sender, EventArgs e)
@@ -44,7 +103,31 @@ namespace Calculator
 
         private void BtnEqualsClicked(object sender, EventArgs e)
         {
-
+            if (operation == EnumOpertion.None)
+                return;
+            switch (operation)
+            {
+                case EnumOpertion.Add:
+                    result = a + b;
+                    break;
+                case EnumOpertion.Subtract:
+                    result = a - b;
+                    break;
+                case EnumOpertion.Multiply:
+                    result = a * b;
+                    break;
+                case EnumOpertion.Divide:
+                    result = a / b;
+                    break;
+            }
+            btnEqualsClicked = true;
+            Print();
+            btnEqualsClicked = false;
+            a = result;
+            b = 0;
+            result = 0;
+            comma = false;
+            operation = EnumOpertion.None;
         }
 
         private void Print()
