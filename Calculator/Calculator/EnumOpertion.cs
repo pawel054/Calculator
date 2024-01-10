6649a8e7983cb6bf41d106fe31787ead82cc6493
pawel054 <pawel.marcisz1@gmail.com>
2024-01-10 11:13:05 +0100

    dodano widok i obsługę przycisków w xaml.cs

---
+++ Calculator/Calculator/EnumOpertion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public enum EnumOpertion
+    {
+        None,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Square,
+        SquareRoot,
+        Inverse
+    }
+}
--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -10,9 +10,44 @@ namespace Calculator
 {
     public partial class MainPage : ContentPage
     {
+        private EnumOpertion operation = EnumOpertion.None;
+        private bool btnEqualsClicked = false;
+        private bool comma = false;
+        private double width = 0;
+        private double height = 0;
+        private double a = 0;
+        private double b = 0;
+        private double result = 0;
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void BtnNumberClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOperationCLicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCommaClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEqualsClicked(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
